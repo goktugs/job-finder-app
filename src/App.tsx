72e12e56aee22a9e5cbe54b34a244a13692fac8b
goktugs <gokojapan5@gmail.com>
2023-09-12 23:56:34 +0300

    wip: add login page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Jobs from "./pages/Jobs";
 import NotFound from "./pages/NotFound";
+import ProtectedRoute from "./ProtectedRoute";
 
 function App() {
   return (
@@ -14,7 +15,9 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/jobs" element={<Jobs />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path="/jobs" element={<Jobs />} />
+          </Route>
         </Route>
         <Route path="/404" element={<NotFound />} />
         <Route path="*" element={<Navigate to="/404" replace />} />
